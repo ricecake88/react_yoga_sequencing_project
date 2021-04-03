a0648c0428b513ff3199809a51b4262e15b05702
Grace S <vocalise@gmail.com>
2021-04-03 14:03:25 -0700

    Modified User Model

--- yoga_sequences_backend/app/models/user.rb
+++ yoga_sequences_backend/app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-    has_secure_password
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
     has_many :yoga_sequences
     has_many :yoga_categories
 end
