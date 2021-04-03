e6ade7602f1f2efff809c26ebcb53771610251e8
Grace S <vocalise@gmail.com>
2021-04-03 14:10:11 -0700

    Remove spaces

--- yoga_sequences_backend/config/routes.rb
+++ yoga_sequences_backend/config/routes.rb
@@ -28,9 +28,9 @@ Rails.application.routes.draw do
             sessions: "api/v1/users/sessions",
           }
       end
-    end  
+    end
 
-    get '/api/v1/users/current_user', to: 'api/v1/users/current_user#index'    
+    get '/api/v1/users/current_user', to: 'api/v1/users/current_user#index'
   ## For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #namespace :api do
   #  namespace :v1 do
