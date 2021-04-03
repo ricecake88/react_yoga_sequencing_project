5376d8a0ad8788b8dbcc7d48f52fd267f0b55a7d
Grace S <vocalise@gmail.com>
2021-04-03 14:31:52 -0700

    Added ReactJS App and Index files

--- yoga_sequences_frontend/src/App.js
+++ yoga_sequences_frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import NormalRoute from "./components/NormalRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
+import withAuth from './components/auth/withAuth';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar />
+      <Switch>
+          <Route exact path='/' component={NormalRoute} />
+          <Route exact path='/protected_route' component={withAuth(ProtectedRoute)} />
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/login' component={Login} />
+      </Switch>
+      </Router>
     </div>
   );
 }
