cc5083ec71b792df9a817498a235622570ee1908
Grace S <vocalise@gmail.com>
2021-04-08 20:59:51 -0700

    Modify migration tags to be an array in yoga poses

--- yoga_sequences_backend/db/migrate/20210330061933_create_yoga_poses.rb
+++ yoga_sequences_backend/db/migrate/20210330061933_create_yoga_poses.rb
@@ -3,7 +3,7 @@ class CreateYogaPoses < ActiveRecord::Migration[5.2]
     create_table :yoga_poses do |t|
       t.string :name
       t.string :sanskrit
-      t.string :tags
+      t.text :tags, array:true
       t.string :category
       t.string :url
       t.string :image
