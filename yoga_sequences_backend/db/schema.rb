1e3b83a3bd0016e41cd5893d50436963826acee2
Grace S <vocalise@gmail.com>
2021-04-14 21:14:36 -0700

    Updated seeds and schema

--- yoga_sequences_backend/db/schema.rb
+++ yoga_sequences_backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_034752) do
+ActiveRecord::Schema.define(version: 2021_04_12_001603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,9 +46,10 @@ ActiveRecord::Schema.define(version: 2021_04_01_034752) do
 
   create_table "sequences", force: :cascade do |t|
     t.string "name"
-    t.integer "category_id"
+    t.integer "category_id", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- yoga_sequences_backend/db/seeds.rb
+++ yoga_sequences_backend/db/seeds.rb
@@ -6,6 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(:name => "Grace", :password => "password", :email => "vocalise@gmail.com")
-YogaPose.create(:name => "Warrior I")
-YogaPose.create(:name=> "Warrior II")
+Pose.create!(:id => 0, :name => "Warrior I", :sanskrit => "Virabhadrasana I",
+    :pose_category => "Heart Opening", :tags => ["Hip", "Back", "Legs", "Glutes"],
+    :url => "Warrior I URL", :video => "Warrior I Video",
+    :image => "Warrior I Image",
+    :description => "Start in Downward Facing Dog. Set Your Right Foot at a 45 degree angle and
+    bring your opposite foot to meet your hands. You can use your hands to help bring the opposite
+    foot if you have difficulty. Repeat on the opposite side.")
+Pose.create!(:id => 1, :name=> "Warrior II", :sanskrit => "Virabhadrasana II",
+    :pose_category=> "Hamstrings", :tags => ["Hip Flexors", "Back", "Glutes"],
+    :url => "Warrior II URL", :video => "Warrior II Video",
+    :image => "warrior II Image",
+    :description => "Standing upright, with legs wide apart and arms spread apart
+        with feet facing forward. Turn the right foot 90 degrees so it is facing the same
+        direction as your hands. Bend your right leg 90 degrees into a lunge forming a right angle.
+        Turn your left foot slightly inward and make sure the left foot is aligned and grounded
+        in a straight line with the front foot. Engage and straighten
+        your left leg while looking past your right hand. Have your body continue facing forward,
+        engaging your core as you make sure your back is fully aligned over your hip/tucked in.")
+
+Category.create(:id => 0, :name => "Uncategorized")
+#Sequence.create(:name => "Morning Routine", :category_id=>1, :poses => [{id: 1}, {id: 2}, {id: 3}])
