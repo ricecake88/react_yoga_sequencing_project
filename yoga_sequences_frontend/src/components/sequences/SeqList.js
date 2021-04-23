4c5d92c79f0dc7d75a72aec3140ddb756d734b1d
Grace S <vocalise@gmail.com>
2021-04-22 20:36:22 -0700

    Change SeqList to include delete and edit a sequence

--- yoga_sequences_frontend/src/components/sequences/SeqList.js
+++ yoga_sequences_frontend/src/components/sequences/SeqList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link, Route, Router } from 'react-router-dom';
-import Seq from '../sequences/Seq';
 
 class SeqList extends Component {
 
@@ -11,8 +10,11 @@ class SeqList extends Component {
             <div>
             {this.props.sequences.length !== 0 ?
                 this.props.sequences.map(seq => {
-                    {/*return <Link to={`sequence/${seq.id}`} key={seq.id}><Seq sequence={seq} delete={this.props.delete}></Seq></Link>*/}
-                    return <span key={seq.id}><Link to={`sequence/${seq.id}`} >{seq.name}</Link><button onClick={() => this.props.delete(seq.id)}>X</button></span>
+                    return <span key={seq.id}>
+                            <Link to={`sequence/${seq.id}`} >{seq.name}</Link>
+                            <button onClick={() => this.props.delete(seq.id)}>X</button>
+                            <Link to={`sequence/edit/${seq.id}`}>Edit</Link>
+                        </span>
                 }) : null}
             </div>
             )
