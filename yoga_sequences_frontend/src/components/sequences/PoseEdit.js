9e8737cc2666f2171462b50cea067239109ac61d
Grace S <vocalise@gmail.com>
2021-04-26 19:40:39 -0700

    Updated num_breaths to be associated to poseInSeq to a key and defaultValue

--- yoga_sequences_frontend/src/components/sequences/PoseEdit.js
+++ yoga_sequences_frontend/src/components/sequences/PoseEdit.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 
 class PoseEdit extends Component {
 
+    // state = {
+    //     num_breaths: this.props.num_breaths,
+    // }
 
+    // onChange = (event) => {
+    //     this.setState({
+    //         num_breaths: parseInt(event.target.value)
+    //     })
+    // }
     render() {
         console.log("PoseEdit");
-        console.log(this.props)
+        console.log(this.props.poseInSeq)
         return (
         <>
             {this.props.poses.map(pose => {
                 return (pose.id === this.props.poseInSeq.pose_id) ? pose.name : null
-            })}        
-            <span><input type="text" value={this.props.poseInSeq.num_breaths} name="num_breaths" onChange={(event) =>this.props.onChange(event)} onBlur={(event) => this.props.onBlur(event, this.props.index)}></input></span>
+            })}
+            <span><input key={this.props.poseInSeq.num_breaths} type="text" defaultValue={this.props.poseInSeq.num_breaths} name="num_breaths" onChange={(event) =>this.props.onChange(event)} onBlur={(event) => this.props.onBlur(event, this.props.index)}></input></span>
+            {/*<span><input type="text" value={this.props.poseInSeq.num_breaths} name="num_breaths" onChange={(event) =>this.props.onChange(event)}></input></span>*/}
+
             <span><button onClick={(event) => this.props.delete(event, this.props.index, this.props.poseInSeq.id)}>X</button></span>
         </>)
     }
