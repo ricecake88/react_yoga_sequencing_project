953d75e907623eef54ca61e557c82ed4167d9368
Grace S <vocalise@gmail.com>
2021-04-28 20:25:46 -0700

    Updated Link to NavLink

--- yoga_sequences_frontend/src/components/sequences/SeqList.js
+++ yoga_sequences_frontend/src/components/sequences/SeqList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link, Route, Router } from 'react-router-dom';
+import { NavLink, Route, Router } from 'react-router-dom';
 
 class SeqList extends Component {
 
@@ -11,9 +11,9 @@ class SeqList extends Component {
             {this.props.sequences.length !== 0 ?
                 this.props.sequences.map(seq => {
                     return <span key={seq.id}>
-                            <Link to={`sequence/${seq.id}`} >{seq.name}</Link>
+                            <NavLink to={`sequence/${seq.id}`} >{seq.name}</NavLink>
                             <button onClick={() => this.props.delete(seq.id)}>X</button>
-                            <Link to={`sequence/edit/${seq.id}`}>Edit</Link>
+                            <NavLink to={`sequence/edit/${seq.id}`}>Edit</NavLink>
                         </span>
                 }) : null}
             </div>
