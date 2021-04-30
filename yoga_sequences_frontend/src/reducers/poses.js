f01f463d9557a60f096f7f08fc723da85399389a
Grace S <vocalise@gmail.com>
2021-04-29 23:26:20 -0700

    Comment out console.logs

--- yoga_sequences_frontend/src/reducers/poses.js
+++ yoga_sequences_frontend/src/reducers/poses.js
@@ -3,9 +3,9 @@ export default function poseReducer(state =
         poses: [], 
         requesting: false
     }, action) {
-    console.log(">>> poseReducer");
-    console.log(state);
-    console.log(action);
+    //console.log(">>> poseReducer");
+    //console.log(state);
+    //console.log(action);
     switch(action.type) {
         case 'START_GET_POSES':
             return {
