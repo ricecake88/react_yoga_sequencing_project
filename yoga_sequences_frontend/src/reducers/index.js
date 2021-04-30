2d9648a5e8e562cdd2291a340056a9265bc5c2de
Grace S <vocalise@gmail.com>
2021-04-29 23:27:33 -0700

    Updated reducer for poseInSeq

--- yoga_sequences_frontend/src/reducers/index.js
+++ yoga_sequences_frontend/src/reducers/index.js
@@ -3,12 +3,14 @@ import authReducer from './auth';
 import categoryReducer from './categories';
 import poseReducer from './poses';
 import sequenceReducer from './sequences';
+import poseInSeqReducer from './poseInSeq';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     categories: categoryReducer,
     poses: poseReducer,
-    sequences: sequenceReducer
+    sequences: sequenceReducer,
+    poseInSequences: poseInSeqReducer
 })
 
 export default rootReducer;
