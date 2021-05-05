76e47265426fbc1b8ed63d487b66e7bff3b11cde
Grace S <vocalise@gmail.com>
2021-05-04 21:38:21 -0700

    Added styling and code that will show what pose user is currently on

--- yoga_sequences_frontend/src/components/sequences/PoseList.js
+++ yoga_sequences_frontend/src/components/sequences/PoseList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const PoseList = ({posesInSeq, poses}) => {
+const PoseList = ({posesInSeq, poses, current}) => {
     console.log("PoseList");
     const posesAll = posesInSeq.map(poseInSeq => {
             return {
@@ -13,8 +13,14 @@ const PoseList = ({posesInSeq, poses}) => {
     console.log(posesAll);
 
     return (
-        <><p>List of Poses</p>
-        {posesAll.map((pose, index) => <div id={"pose" + pose.id} key={"pose" + pose.id}>#{index+1} {pose.name} Number of Breaths: {pose.num_breaths}</div>)}
+        <>
+        <p>List of Poses</p>
+            {posesAll.map((pose, index) => {
+                return current === index ?
+                    <div id={"pose" + pose.id} key={"pose" + pose.id} className="selectedPose">#{index+1} {pose.name} Number of Breaths: {pose.num_breaths}</div>
+                    :
+                    <div id={"pose" + pose.id} key={"pose" + pose.id}>#{index+1} {pose.name} Number of Breaths: {pose.num_breaths}</div>
+            })}
         </>
     )
 }
