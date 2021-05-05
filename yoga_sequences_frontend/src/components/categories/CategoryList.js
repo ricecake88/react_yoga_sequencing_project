3e584b31969f0196e3b74b936f89a006b7d4930e
Grace S <vocalise@gmail.com>
2021-05-04 21:32:58 -0700

    Add checking to see if categories exist before rendering

--- yoga_sequences_frontend/src/components/categories/CategoryList.js
+++ yoga_sequences_frontend/src/components/categories/CategoryList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Category from './Category';
 
 const CategoryList = (props) => {
-    return props.categories.map(category => <Category key={category.id} category={category} deleteCategory={props.deleteCategory}/>)
+    return props.categories.length !== 0? props.categories.map(category => <Category key={category.id} category={category} deleteCategory={props.deleteCategory}/>) : null
 }
 
 export default CategoryList;
