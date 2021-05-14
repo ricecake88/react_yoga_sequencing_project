63fde046cfd9383cd4293b550ab1141864432f85
Grace S <vocalise@gmail.com>
2021-05-14 13:35:46 -0700

    Remove checking if user is authenticated to view poses

--- yoga_sequences_backend/app/controllers/api/v1/poses_controller.rb
+++ yoga_sequences_backend/app/controllers/api/v1/poses_controller.rb
@@ -1,23 +1,17 @@
 class Api::V1::PosesController < ApplicationController
-    before_action :authenticate_user!
 
     def index
-
-        if current_user
             poses = Pose.all
             render json: {poses: poses}
-        end
     end
 
     def show
-        if current_user
             pose = Pose.find(:id => params[:id])
             if pose
                 render json: {pose: pose}
             else
                 render json: {error: "Pose not found."}, status: 400
             end
-        end
     end
 
 end
