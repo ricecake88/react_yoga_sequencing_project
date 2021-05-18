7ccf64c3474cd07d78dcdda929142ee6ea7ff958
Grace S <vocalise@gmail.com>
2021-05-17 21:40:04 -0700

    Update error action

--- yoga_sequences_frontend/src/actions/errors.js
+++ yoga_sequences_frontend/src/actions/errors.js
@@ -4,15 +4,17 @@ export const clearErrorMessage =() => {
     return (dispatch) => dispatch({type: 'CLEAR_ERROR'})
 }
 
+export const setError = (msg) => {
+    return (dispatch) => dispatch({type: 'ERROR', error: msg})
+}
+
 export const handleServerError = (response, dispatch) => {
     return response.json().then(json => {
-        console.log("in handleServerError")
-        console.log(response)
         dispatch({type: 'ERROR', error: json.error})
         if (response.status === 401)
             return Promise.reject(dispatch({type: NOT_AUTHENTICATED}))
         else 
-            //Promise.reject(dispatch({type: 'ERROR', error: json.error})).catch(err => console.log("Caught Promise!"))           
+        
             return Promise.reject(dispatch({type: 'ERROR', error: json.error}))
         })
 
