54cccea0cf19f858b100817642c3ef22a8e52d7a
Grace S <vocalise@gmail.com>
2021-05-17 22:03:13 -0700

    Cleaned up App.js

--- yoga_sequences_frontend/src/App.js
+++ yoga_sequences_frontend/src/App.js
@@ -2,16 +2,12 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import NormalRoute from "./components/NormalRoute";
-//import AllSequences from "./components/AllSequences";
 import Signup from "./components/auth/Signup";
 import Login from "./components/auth/Login";
 import withAuth from './components/auth/withAuth';
-//import SeqContainer from './containers/SeqContainer';
 import SeqListContainer from './containers/SeqListContainer';
 import CategoryContainer from './containers/CategoryContainer';
 import PoseContainer from './containers/PoseContainer';
-//import SeqShow from './components/sequences/SeqShow';
-//import SeqFormNew from './components/sequences/SeqFormNew';
 import SeqFormContainer from './containers/SeqFormContainer';
 import SeqShowContainer from './containers/SeqShowContainer';
 
@@ -25,8 +21,6 @@ function App() {
         <div className="genericInnerContainer">
           <Switch>
               <Route exact path='/' component={NormalRoute} />
-              {/*<Route exact path='/sequences' component={withAuth(AllSequences)}/>*/}
-              {/*<Route exact path='/sequences/new' component={withAuth(SeqContainer)}/>*/}
               <Route exact path='/categories' component={withAuth(CategoryContainer)}/>
               <Route exact path='/poses' component={PoseContainer}/>
               <Route exact path='/signup' component={Signup} />
