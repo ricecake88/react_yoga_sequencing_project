59fe24bd8d495d0b9382a2d8109cfa876724ecb3
Grace S <vocalise@gmail.com>
2021-05-17 23:21:47 -0700

    Update and checkin schema.rb

--- yoga_sequences_backend/db/schema.rb
+++ yoga_sequences_backend/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_001603) do
+ActiveRecord::Schema.define(version: 2021_04_13_000000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.integer "user_id"
-    t.string "name"
+    t.string "name", default: "Uncategorized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
