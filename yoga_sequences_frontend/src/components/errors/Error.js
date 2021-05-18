8702208430c4f10ecca0eb12e5bf3a375bda2aa3
Grace S <vocalise@gmail.com>
2021-05-17 21:39:50 -0700

    Updated style for Error component

--- yoga_sequences_frontend/src/components/errors/Error.js
+++ yoga_sequences_frontend/src/components/errors/Error.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Error = (props) => {
     let message = ''
     if (props !== null && props.error !== null) {
-        message = props.error
+        message = "*" + props.error
     }
     return <>
-       { message }
+       <div className="error margin10">{ message }</div>
     </>
     
 }
