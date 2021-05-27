7d03306ac7603610747d2b42f2ba638d8a70346f
Grace S <vocalise@gmail.com>
2021-05-26 19:17:14 -0700

    Add index and modify create to return status

--- yoga_sequences_backend/app/controllers/api/v1/pose_in_seqs_controller.rb
+++ yoga_sequences_backend/app/controllers/api/v1/pose_in_seqs_controller.rb
@@ -2,11 +2,16 @@ class Api::V1::PoseInSeqsController < ApplicationController
 
     before_action :authenticate_user!
 
+    def index 
+        poses = PoseInSeq.where(:sequence => params[:sequence_id])
+        render json: {pose_in_seqs: poses}, status: 200
+    end
+
     def create
         Rails.logger.debug params.inspect
         pose = PoseInSeq.new(pose_in_seq_params)
         if pose.save!
-            render json: { pose: pose }
+            render json: { pose: pose }, status: 200
         else
             render json: { error: pose.errors }, status: 422
         end
