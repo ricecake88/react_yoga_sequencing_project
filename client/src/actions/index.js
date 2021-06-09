e72f7eaab0ef713822aad4aba787591f7386ab73
Grace S <vocalise@gmail.com>
2021-06-09 00:44:42 -0700

    Updated URLs again

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,8 @@
 export const AUTHENTICATED = 'AUTHENTICATED'
 export const NOT_AUTHENTICATED = 'NOT_AUTHENTICATED'
-export const BACKEND_URL_BASE = `/`
-export const BACKEND_USERS_URL = `/users`
-export const BACKEND_URL = `/`
+/*export const BACKEND_URL_BASE = `http://localhost:3001`
+export const BACKEND_USERS_URL = `http://localhost:3001/api/v1/users`
+export const BACKEND_URL = `http://localhost:3001/api/v1`*/
+export const BACKEND_URL_BASE = `https://yoga-sequencing-app.herokuapp.com`
+export const BACKEND_USERS_URL = `https://yoga-sequencing-app.herokuapp.com/api/v1/users`
+export const BACKEND_URL = `https://yoga-sequencing-app.herokuapp.com/api/v1`
