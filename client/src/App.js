308069027f0166b3bdfbb8e6ef3c1c1eb46a900c
Grace S <vocalise@gmail.com>
2021-06-09 23:51:46 -0700

    Added basename to Router

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
+      <Router basename="/client">
       <Navbar />
       <div className="genericContainer">
         <div className="genericInnerContainer">
