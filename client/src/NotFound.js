5dab13752998cd78309b320db2a061434cb8ad4b
Grace S <vocalise@gmail.com>
2021-06-09 22:55:45 -0700

    Fixed Home Component and Not Found Component

--- client/src/NotFound.js
+++ client/src/NotFound.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-import { Container, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 class NotFound extends Component {
   render () {
-    return <Container text textAlign='center'>
+    return <>
       <h1>404: Not found</h1>
-      <Button as={Link} to='/'>Back to home</Button>
-    </Container>
+      </>
   }
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function NormalRoute() {
+export default function Home() {
   return (
     <div>
       <div>Welcome to Yoga Sequencing!</div>
